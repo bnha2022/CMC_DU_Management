c917c4e6a42899a520caa87976c855c922763d0e
bnha22 <bnha@cmcglobal.vn>
2022-02-25 09:04:46 +0700

    Rename application

--- src/main/java/cmc/du21/cms/CmcDUManagementApplication.java
+++ src/main/java/cmc/du21/cms/CmcDUManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CmcEmployeeManagementApplication {
+public class CmcDUManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CmcEmployeeManagementApplication.class, args);
+        SpringApplication.run(CmcDUManagementApplication.class, args);
     }
 
 }
